ed32510b02e82a06d7e96fc21a90a8d10e8b33f8
vapb <vapb7123@gmail.com>
2022-07-14 22:03:44 -0300

    Create Real data to insert into DB

---
+++ InsertData_script.sql
@@ -0,0 +1,11 @@
+-- Customer Data (cust_id, username, password, firstname, surname, birthday)
+
+
+INSERT INTO customer (username, password, firstname, surname, birthday) VALUES
+    ('B6717', 'Tampopo', 110, '1985-02-10', 'Comedy'),
+    ('HG120', 'The Dinner Game', 140, DEFAULT, 'Comedy');
+
+
+-- Cupom Data
+
+-- Restaurant Data
